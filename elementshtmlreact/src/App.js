0b24f1596780e0262972d45edc1181b065886be7
alexisdmc21 <admorales7@espe.edu.ec>
2025-07-29 10:41:00 -0500

    feat: agregar componentes en App.js
    Se agrega los componentes solicitados sin estilos de forma basica

--- elementshtmlreact/src/App.js
+++ elementshtmlreact/src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import Imagen from "./components/ImagenElement"
+import Titulo from "./components/TituloElement";
+import Parrafo from "./components/ParrafoElement";
+import Enlace from "./components/EnlaceElement";
+import Lista from "./components/ListaElement";
+import Tabla from "./components/TablaElement";
+import Formulario from "./components/FormElement";
+import Boton from "./components/ButtonElement";
+import Input from "./components/InputElement";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+      {/*ELEMENTOS BASICOS*/}
+      <Titulo texto="ELEMENTOS BASICOS" nivel="1"/>
+      <div>
+        <Imagen src="https://th.bing.com/th/id/R.53cdc1d97c02399451d30432a3c4c08f?rik=kMpEugC8LbrV7A&pid=ImgRaw&r=0" alt="espe" width="800px" height="auto"/>
+      </div>
+      <div>
+        <Titulo texto="Esta es una etiqueta H1" nivel="1"/>
+        <Titulo texto="Esta es una etiqueta H2" nivel="2"/>
+        <Titulo texto="Esta es una etiqueta H3" nivel="3"/>
+        <Titulo texto="Esta es una etiqueta H4" nivel="4"/>
+        <Titulo texto="Esta es una etiqueta H5" nivel="5"/>
+        <Titulo texto="Esta es una etiqueta H6" nivel="6"/>
+      </div>
+      <div>
+        <Parrafo texto="Parrafo" />
+      </div>
+      <div>
+        <Enlace href="#" texto="ESTE ES UN ENLACE"/>
+      </div>
+
+      {/*LISTAS Y TABLAS*/}
+      <div>
+        <Lista elementos={["Primer elemento", "Segundo elemento", "Tercero elemento", "Cuarto elemento"]}/>
+      </div>
+      <div>
+        <Tabla cabecera={['Nombre', 'Apellido', 'ID', 'Cedula', 'Nivel']} 
+        datos={[
+          ['Juan', 'Pérez', 'L00420328', '1750237812', 'Segundo'],
+          ['María', 'González', 'L00567213', '1750849321', 'Primero'],
+          ['Carlos', 'Ramírez', 'L00391827', '1750912834', 'Tercero'],
+          ['Ana', 'Mendoza', 'L00654329', '1750628471', 'Octavo']
+          ]} />
+      </div>
+
+      {/*FORMULARIOS*/}
+      <div>
+        <Formulario>
+          <label>Este es un Formulario con children</label>
+          <input type="text"/>
+          <button>Enviar</button>
+        </Formulario>
+      </div>
+      <div>
+        <Boton onClick={""} texto="Este es mi boton"/>
+      </div>
+      <div>
+        <Input type="text" placeholder="Este es el input generico" value="" onChange=""/>
+      </div>
+      </div>
     </div>
   );
 }
