6600c2994398ada80b09c8f48590dcf724a7a8aa
alexisdmc21 <admorales7@espe.edu.ec>
2025-07-29 10:45:43 -0500

    feat: Crear InputElement.js
    Se crea el input generico que servira para ser reutilizado para el resto de inputs de diferentes tipos

---
+++ elementshtmlreact/src/components/InputElement.js
@@ -0,0 +1,14 @@
+function Input(props){
+    return(
+        <div>
+            <input 
+            type={props.type}
+            placeholder={props.placeholder}
+            value={props.value}
+            onChange={props.onChange}
+            />
+        </div>
+    );
+}
+
+export default Input;
