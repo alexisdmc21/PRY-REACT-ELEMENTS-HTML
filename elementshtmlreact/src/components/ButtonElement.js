29e409c133d669025bfc709bb18891739114de1f
alexisdmc21 <admorales7@espe.edu.ec>
2025-07-30 21:50:13 -0500

    fix: agregacion de estilosen ButtonElement.js

--- elementshtmlreact/src/components/ButtonElement.js
+++ elementshtmlreact/src/components/ButtonElement.js
@@ -1,6 +1,8 @@
 function Boton(props){
     return(
-        <button onClick={props.onClick}>{props.texto}</button>
+        <button className="btn btn-primary btn-outline-success text-black btn-lg px-5 border-0" onClick={props.onClick}>
+            {props.texto}
+            </button>
     )
 }
 
