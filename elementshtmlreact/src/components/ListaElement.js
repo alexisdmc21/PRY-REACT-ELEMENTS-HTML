3d000aff6486fae908ea5f3eb1a6ae12b5dfad0d
alexisdmc21 <admorales7@espe.edu.ec>
2025-07-30 21:59:42 -0500

    fix: delet divs

--- elementshtmlreact/src/components/ListaElement.js
+++ elementshtmlreact/src/components/ListaElement.js
@@ -2,13 +2,11 @@ import React from "react";
 
 function Lista(props){
     return(
-        <div>
            <ul>
             {props.elementos.map((elemento, index) => (
                 <li key={index}>{elemento}</li>
             ))}
             </ul> 
-        </div>
     );
 }
 
