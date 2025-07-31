2f9fe8b5b14fb00bd5492addbcb5251b842aef0b
alexisdmc21 <admorales7@espe.edu.ec>
2025-07-30 22:01:06 -0500

    feat: agregar estilos en table
    Se agrega estilos a la tabla

--- elementshtmlreact/src/components/TablaElement.js
+++ elementshtmlreact/src/components/TablaElement.js
@@ -1,25 +1,42 @@
 import React from "react";
 
+
 function Tabla(props) {
+
+  /*CONSTANTES QUE GUARDAN LOS DATOS QUE MOSTRARA EN LA TABLA*/
   const cabecera = props.cabecera || [];
   const datos = props.datos || [];
   
   return (
+
+    /*SE GENERA LA TABLA DE ACUERDO A LOS DATOS INGRESADOS A LOS PROPS*/
     <table className="table">
       {cabecera.length > 0 && (
-        <thead>
-          <tr>
+
+        /*SE DA ESTILOS POR CADA COMPONENTE DE LA TABLA*/
+        <thead className="table-primary border-1 border-black">
+          <tr className="border-bottom border-black">
+
+            {/*MEDIANTE EL METODO MAP RECORRE EL ARREGLO, EN ESTE CASO DE LA CABECERA*/}
             {cabecera.map((columna, index) => (
-              <th key={index} scope="col">{columna}</th>
+              <th key={index} scope="col" className="text-center py-3">
+                {columna}
+              </th>
             ))}
           </tr>
         </thead>
       )}
+
+      {/*CUERPO DE LA TABLA*/}
       <tbody>
+
+        {/*DEL MISMO MODO CON EL METODO MAP RECORRE EL ARRAY DE LOS DATOS Y LOS VA UBICANDO EN LA TABLA*/}
         {datos.map((fila, filaIndex) => (
-          <tr key={filaIndex}>
+          <tr key={filaIndex} className="border-bottom border-1 border-black">
             {fila.map((celda, celdaIndex) => (
-              <td key={celdaIndex}>{celda}</td>
+              <td key={celdaIndex} className="text-center py-2">
+                {celda}
+              </td>
             ))}
           </tr>
         ))}
